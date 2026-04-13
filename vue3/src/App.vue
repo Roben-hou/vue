d46ed8ab0ed5fb1aeb0132f7e6f7f3a18dc22645
Roben-hou <coppinkausar@gmail.com>
2026-04-13 22:35:07 +0800

    事件委托

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -6,10 +6,11 @@
 //import Test4 from './views/vue基本语法/1.模版语法/4.列表渲染.vue'
 //import Test5 from './views/vue基本语法/1.模版语法/5.事件绑定.vue'
 //import Test6 from './views/vue基本语法/1.模版语法/6.prevent和once.vue'
-import Test7 from './views/vue基本语法/1.模版语法/7.事件冒泡和阻止事件冒泡.vue'
+//import Test7 from './views/vue基本语法/1.模版语法/7.事件冒泡和阻止事件冒泡.vue'
+import Test8 from './views/vue基本语法/1.模版语法/8.事件委托.vue'
 </script>
 
 <template>
   <!-- 直接像写 HTML 标签一样把它放在页面上！ -->
-<Test7/>
+<Test8/>
 </template>
---
+++ vue3/src/views/vue基本语法/1.模版语法/8.事件委托.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+function clickitem(item:number) {
+    console.log(item)
+}
+function clickitem1(event:Event) {
+    const target = event.target as HTMLElement
+      const item = target.closest('.item') as HTMLElement;
+    
+    if (item) {
+        const index = item.dataset.index;
+        console.log("事件委托捕获到点击：", index);
+    }
+
+}
+</script>
+
+<template>
+<div class="list">
+    <div @click="clickitem(item)" class="item" v-for="item in 20" >{{ item }}</div>
+</div>
+
+<div class="list" @click="clickitem1">
+    <div  class="item" v-for="item in 20" :data-index="item">{{ item }}</div>
+</div>
+</template>
+
+<style>
+.list{
+    width: 500px;
+    height: 500px;
+    display: grid;
+    grid-template-columns: repeat(5,1fr);
+    padding: 20px;
+    gap:20px;
+    margin-bottom: 30px;
+    background-color: blanchedalmond;
+    .item{
+        width: 50px;
+        height: 50px;
+        background-color: #fff;
+        border-radius: 20px;
+        padding: 5px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+}
+</style>
+
