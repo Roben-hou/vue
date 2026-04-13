bec2dcb5bc0cc24ce9701f30f94173fef1571e00
Roben-hou <coppinkausar@gmail.com>
2026-04-13 15:44:46 +0800

    事件冒泡

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -5,10 +5,11 @@
 //import Test3 from './views/vue基本语法/1.模版语法/3条件渲染.vue'
 //import Test4 from './views/vue基本语法/1.模版语法/4.列表渲染.vue'
 //import Test5 from './views/vue基本语法/1.模版语法/5.事件绑定.vue'
-import Test6 from './views/vue基本语法/1.模版语法/6.prevent和once.vue'
+//import Test6 from './views/vue基本语法/1.模版语法/6.prevent和once.vue'
+import Test7 from './views/vue基本语法/1.模版语法/7.事件冒泡和阻止事件冒泡.vue'
 </script>
 
 <template>
   <!-- 直接像写 HTML 标签一样把它放在页面上！ -->
-<Test6/>
+<Test7/>
 </template>
---
+++ vue3/src/views/vue基本语法/1.模版语法/7.事件冒泡和阻止事件冒泡.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+function clickhandler(msg:string){
+    console.log(msg)
+}
+</script>
+
+<template>
+    <div>事件冒泡</div>
+<div class="A" @click="clickhandler('A')">
+    <div class="B" @click="clickhandler('B')">
+        <button @click="clickhandler('button')">button</button>
+    </div>
+</div>
+<div>stop阻止事件冒泡</div>
+<div class="A" @click.stop="clickhandler('A')">
+    <div class="B" @click.stop="clickhandler('B')">
+        <button @click.stop="clickhandler('button')">button</button>
+    </div>
+</div>
+<div>self只触发自己的事件</div>
+<div class="A" @click="clickhandler('A')">
+    <div class="B" @click.self="clickhandler('B')">
+        <button @click.self="clickhandler('button')">button</button>
+    </div>
+</div>
+</template>
+
+<style>
+
+.A{
+    width: 400px;
+    height: 200px;
+    background-color: cadetblue;
+    padding: 40px;
+    margin-bottom: 30px;
+    margin-left: 30px;
+    border-radius: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .B{
+        width: 200px;
+        height: 100px;
+        background-color: rgb(135, 156, 230);
+        padding: 20px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 20px;
+        button{
+            width: 100px;
+            height: 50px;
+            border-radius: 20px;
+        }
+    }
+}
+
+
+</style>
