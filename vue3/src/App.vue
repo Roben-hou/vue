fe83d2f297f00cf63f8e4cc402f3bf7834032516
Roben-hou <coppinkausar@gmail.com>
2026-04-16 23:27:31 +0800

    ref1

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -7,10 +7,10 @@
 //import Test5 from './views/vue基本语法/1.模版语法/5.事件绑定.vue'
 //import Test6 from './views/vue基本语法/1.模版语法/6.prevent和once.vue'
 //import Test7 from './views/vue基本语法/1.模版语法/7.事件冒泡和阻止事件冒泡.vue'
-import Test8 from './views/vue基本语法/1.模版语法/8.事件委托.vue'
+import Test9 from './views/vue基本语法/1.模版语法/9.ref1.vue'
 </script>
 
 <template>
   <!-- 直接像写 HTML 标签一样把它放在页面上！ -->
-<Test8/>
+<Test9/>
 </template>
