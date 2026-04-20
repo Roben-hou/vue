11b0359d85a1a3ff0cf04bf300a852d6aeddc146
Roben-hou <coppinkausar@gmail.com>
2026-04-20 20:06:37 +0800

    watch4

---
+++ vue3/src/views/vue基本语法/1.模版语法/19.watch4.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import { reactive, ref, watch } from 'vue';
+let person = ref({
+    name: 'nagi',
+    age: 20  ,
+    job: {
+        type:'student',
+        salary:'0k'
+    }
+})
+function changename() {
+    person.value.name+='~'
+}
+function changeage() {
+    person.value.age+=1
+}
+function changetype() {
+    person.value.job.type='programmer'
+}
+function changesalary() {
+    person.value.job.salary='20k'
+}
+function changejob() {
+    person.value.job={type:'teacher',salary:'10k'}
+}
+// 监听响应式对象中的某个属性,且该属性是基本数据类型，用函数形式
+// watch(()=>person.value.name,(value)=>{
+//     console.log('namechange',value)
+// }
+// )
+watch(()=>person.value.job,(value)=>{
+    console.log('jobchange',value)
+},{deep:true}
+)
+</script>
+
+<template>
+    <div class="person">
+    <h2>情况3:监听reactive定义的对象数据</h2>
+    <h3>name:{{ person.name }}</h3>
+    <h3>age:{{ person.age }}</h3>
+    <h3>job.type:{{ person.job.type }}</h3>
+    <h3>job.salary:{{ person.job.salary }}</h3>
+    <button @click="changename">changename</button>
+    <button @click="changeage">changeage</button>
+    <button @click="changetype">changetype</button>
+    <button @click="changesalary">changesalary</button>
+    <button @click="changejob">changejob</button>
+    </div>
+</template>
+
+<style>
+.person {
+  background-color: #4ba0dd;
+  box-shadow: 0 0 10px;
+  padding: 20px;
+  border-radius: 10px;
+}
+button{
+    margin: 0 5px;
+}
+</style>
+
